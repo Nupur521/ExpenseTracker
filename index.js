b3c61012163617eff34e949c4667e1bd079ed5ea
Nupur <nupuragarwal521@gmail.com>
2022-09-16 11:02:44 +0530

    delete functionality added

--- index.js
+++ index.js
@@ -3,13 +3,67 @@ let income = document.getElementById("showIncome");
 let expense = document.getElementById("showExpense");
 let itemsList = document.getElementById("history");
 let addTransBtn = document.getElementById("addTransaction");
-
+let newItem = document.getElementById("addItem");
+let newItemAmount = document.getElementById("amount");
 
 displayList();
-addTransBtn.addEventListener("click", () => {
-    let newItem = document.getElementById("addItem");
-    let newItemAmount = document.getElementById("amount");
 
+//Display the list on the screen
+function displayList() {
+    let html = "";
+    let allItems = localStorage.getItem("allItems");
+    if (allItems == null)
+        items = [];
+    else
+        items = JSON.parse(allItems);
+
+    if (items.length == 0) {
+        console.log(itemsList.innerHTML)
+        html += `<div class="noItems">No new items to display<br>Add items from below</div>`
+        itemsList.innerHTML = html;
+    } else {
+        items.forEach((element, index) => {
+
+            if (Number(element.amount) < 0)
+                html += ` <div class="addedClass debit"><span class="item">${element.item}</span><span class="money">${Number(element.amount)}</span><button
+                    class="deleteBtn" id=${index} onclick="deleteElm(this.id)">x</button></div>`;
+            else if (Number(element.amount) > 0)
+                html += `<div class="addedClass credit"><span class="item">${element.item}</span><span class="money">${Number(element.amount)}</span><button
+                    class = "deleteBtn"  id=${index} onclick = "deleteElm(this.id)"> x </button></div > `;
+
+        });
+    }
+
+    itemsList.innerHTML = html;
+}
+
+//delete something from the list
+
+function deleteElm(index) {
+    items.splice(index, 1);
+    localStorage.setItem("allItems", JSON.stringify(items));
+    displayList();
+}
+
+//transfer focus to other input box on enter
+newItem.addEventListener("keyup", (e) => {
+    if (e.key == 'Enter')
+        newItemAmount.focus();
+});
+
+
+
+//When the transaction button is fired on keyboard event or clicked manually
+newItemAmount.addEventListener("keyup", (e) => {
+    if (e.key == 'Enter')
+        TransactionFnc();
+})
+addTransBtn.addEventListener("click", () => {
+    TransactionFnc();
+})
+
+//on the firing of addTransaction button
+function TransactionFnc() {
     let listItem = {
         item: newItem.value,
         amount: newItemAmount.value
@@ -27,33 +81,5 @@ addTransBtn.addEventListener("click", () => {
     newItemAmount.value = "";
 
     displayList();
-})
-
-
-function displayList() {
-    let html = "";
-    let allItems = localStorage.getItem("allItems");
-    if (allItems == null)
-        items = [];
-    else {
-        items = JSON.parse(allItems);
-    }
-
-    if (items.length == 0)
-        itemsList.innerHTML = `<div> nothing to display</div>`
-
-    else {
-        items.forEach((element, index) => {
-
-            if (Number(element.amount) < 0)
-                html += ` <div class="addedClass debit"><span class="item">${element.item}</span><span class="money">${Number(element.amount)}</span><button
-                    class="deleteBtn">x</button></div>`;
-            else if (Number(element.amount) > 0)
-                html += `<div class="addedClass credit"><span class="item">${element.item}</span><span class="money">${Number(element.amount)}</span><button
-                    class="deleteBtn">x</button></div>`;
-
-        });
-    }
-
-    itemsList.innerHTML = html;
+    newItem.focus();
 }
